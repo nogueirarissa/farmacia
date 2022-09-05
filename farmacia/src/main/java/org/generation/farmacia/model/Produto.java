5ff0849130e8dadceca6fca4501aa25f9394623b
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-05 15:36:55 -0300

    Cria relacionamento ManyToOne entre as entidades

--- farmacia/src/main/java/org/generation/farmacia/model/Produto.java
+++ farmacia/src/main/java/org/generation/farmacia/model/Produto.java
@@ -4,8 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
 
 @Entity
 @Table(name = "tb_produtos")
@@ -28,9 +33,21 @@ public class Produto {
 	private int quantidade;
 	
 	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public Long getId() {
 		return id;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 	public void setId(Long id) {
