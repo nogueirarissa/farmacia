5c747701772cebd170dd35e73777c54138def9f0
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-05 14:39:07 -0300

    Modifica o Request Mapping

--- farmacia/src/main/java/org/generation/farmacia/controller/CategoriaController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/CategoriaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorias")
 @CrossOrigin(origins = "*")
 public class CategoriaController {
 
