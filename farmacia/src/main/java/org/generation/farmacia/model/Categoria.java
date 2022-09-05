815b6bbafe9e0ca0815050d3593c55bf6ed0c309
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-05 13:33:43 -0300

    Adiciona o Recurso Categoria à camada Model

---
+++ farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
@@ -0,0 +1,43 @@
+package org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String tipo;
+	
+	@NotNull
+	private String subtipo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getSubtipo() {
+		return subtipo;
+	}
+	public void setSubtipo(String subtipo) {
+		this.subtipo = subtipo;
+	}
+
+}
