7ed77a81d276ebdbd86b548e8a99175d61fe306c
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-05 15:35:59 -0300

    Cria relacionamento OneToMany entre as entidades

--- farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
+++ farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
@@ -1,11 +1,17 @@
 package org.generation.farmacia.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categorias")
@@ -21,6 +27,16 @@ public class Categoria {
 	@NotNull
 	private String subtipo;
 	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto>produto;
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 	public Long getId() {
 		return id;
 	}
